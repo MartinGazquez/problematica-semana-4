f442d4b9ddb2c9d7a3e0ed793c9c8643a6fdc5a0
Martin Gazquez <lechu@SALDIVIA-DOMAIN.LAN>
2020-10-26 14:40:52 -0300

    end part 1

---
+++ week4.js
@@ -0,0 +1,21 @@
+// 1)Variables y Operadores
+//a)
+var a= 1
+var b= 2
+var suma = (a + b)
+console.log (suma);
+
+//b)
+var i= "joven"
+var b= "feliz"
+var oracion = (i + b)
+console.log (oracion);
+
+//c)
+var i= "joven"
+var b= "feliz"
+var oracion = (i.length + b.length)
+console.log (oracion);
+
+//2)Strings
+//a)
