b02a59c78d911a8c7aaf5a00d3fa80ec48df0af1
Martin Gazquez <lechu@SALDIVIA-DOMAIN.LAN>
2020-10-26 15:46:00 -0300

    end half part 2

--- week4.js
+++ week4.js
@@ -19,3 +19,24 @@ console.log (oracion);
 
 //2)Strings
 //a)
+var word= "codificacion"
+var m= word.toUpperCase ();
+console.log (m);
+
+//b)
+var name= "codificacion"
+var piece= name.substring (0, 5);
+console.log (piece);
+
+//c)
+var name= "codificacion"
+var last= name.substring (9);
+console.log (last);
+
+//d)
+var name= "codificacion"
+var change= name.substring (0, 1);
+var changema= name.toUpperCase ();
+var changemi= name.tolower (1, 12);
+var all= change+changema+changemi;
+console.log (all);
